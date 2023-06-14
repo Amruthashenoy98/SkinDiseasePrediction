0891076aa7f649ad81f3db3b17fed9586fcf46d0
amrutha <shenoyamrutha98@gmail.com>
2023-06-14 12:40:55 +0530

    first commit file structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+name='skindiseaseprediction',
+version='0.0.1',
+author='Amrutha',
+author_email='shenoyamrutha98@gmail.com',
+packages=find_packages(),
+install_requires=[],
+
+)
